885463caf5558d6839870bd54063168bcfb02fac
K-real1995 <garfild17@yandex.ru>
2022-05-10 17:46:06 +0800

    Adding comment to main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,18 +6,23 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
         try {
+            //Создаем подключение к базе данных db_lu
             Connection connection = DriverManager.getConnection("jdbc:sqlite:db_lu");
             System.out.println("Подключение выполнено");
+            //Создаем три запроса на создание таблиц
             String query = "CREATE TABLE 'users'(name char," +
                     "surname char," +
                     "money integer," +
                     "birthday datetime);";
+
             String query2 = "CREATE TABLE 'cars'(name char," +
                     "color char," +
                     "model char," +
                     "speed char);";
+
             String query3 = "CREATE TABLE 'shops'(name char," +
                     "address char);";
+            // Обновляем нашу базу данных db_lu тремя таблицами
             Statement statement = connection.createStatement();
             statement.executeUpdate(query);
             statement.executeUpdate(query2);
